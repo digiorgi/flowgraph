ed417d3b206aebd4c57b2dd8f8f2b2d0e43a0851
Raymond Ellis <raymondellis@live.com>
2010-12-04 08:49:47 -0800

    Added MIDI Volume   Sets the volume of all channels.

---
+++ bin/Plugins/MIDI/MIDI_Volume.vb
@@ -0,0 +1,94 @@
+﻿'AddMenuObject|Volume,Plugins.MIDI_Volume|MIDI
+Public Class MIDI_Volume
+    Inherits BaseObject
+
+    Private Enabled As Boolean = True
+
+    Private numVolume As New NumericUpDown
+
+#Region "Object stuff"
+    Public Sub New(ByVal StartPosition As Point, ByVal UserData As String)
+        Setup(UserData, StartPosition, 95) 'Setup the base rectangles.
+
+        'Create one output.
+        Outputs(New String() {"Channel Message,ChannelMessageBuilder,ChannelMessage"})
+
+        Inputs(New String() {"Enable,Boolean", "Channel Message,ChannelMessageBuilder,ChannelMessage"})
+
+        'Set the title.
+        Title = "MIDI Volume"
+
+        numVolume.Maximum = 128
+        numVolume.Width = 50
+        numVolume.Value = 128
+        numVolume.Location = Position + New Point(45, 0)
+        AddControl(numVolume)
+
+    End Sub
+
+    Public Overrides Sub Dispose()
+        numVolume.Dispose()
+
+        MyBase.Dispose()
+    End Sub
+
+    Public Overrides Sub Moving()
+        numVolume.Location = Position + New Point(45, 0)
+    End Sub
+
+    Public Overrides Sub Receive(ByVal Data As Object, ByVal sender As DataFlow)
+        Select Case sender.Index
+            Case 0 'Enable
+                If Data <> Enabled Then
+                    Enabled = Data
+                End If
+
+
+            Case 1 'Input axis
+                If Not Enabled Then Return
+
+                Dim message As Sanford.Multimedia.Midi.ChannelMessageBuilder
+                If Data.GetType = GetType(Sanford.Multimedia.Midi.ChannelMessage) Then
+                    message = New Sanford.Multimedia.Midi.ChannelMessageBuilder(Data)
+                Else
+                    message = Data
+                End If
+
+
+
+                'Is it a note (on or off)?
+                If (message.Command = Sanford.Multimedia.Midi.ChannelCommand.NoteOn) Then
+                    If message.Data2 > 0 Then
+                        message.data2 = numvolume.value - 1
+                    End If
+                End If
+
+                Send(message)
+        End Select
+    End Sub
+
+    Public Overrides Sub Draw(ByVal g As System.Drawing.Graphics)
+        MyBase.Draw(g)
+
+        g.DrawString("Volume:", DefaultFont, DefaultFontBrush, Position.X, Position.Y)
+    End Sub
+
+    Public Overrides Sub Load(ByVal g As SimpleD.Group)
+
+        g.Get_Value("Enabled", Enabled, False)
+        g.Get_Value("Volume", numVolume.Value, False)
+        MyBase.Load(g)
+    End Sub
+
+    Public Overrides Function Save() As SimpleD.Group
+        Dim g As SimpleD.Group = MyBase.Save()
+
+        g.Set_Value("Enabled", Enabled)
+        g.Set_Value("Volume", numvolume.value)
+
+
+        Return g
+    End Function
+#End Region
+
+End Class
