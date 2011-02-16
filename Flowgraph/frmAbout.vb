78112fa6ac91d4d673402cebb14f73869c719eca
Raymond Ellis <raymondellis@live.com>
2011-02-15 23:05:28 -0800

    I fixed a bug! The Draw checkbox was not disabling All drawing. It was just disabling auto draw.
    Added links to the about form.

--- Flowgraph/frmAbout.vb
+++ Flowgraph/frmAbout.vb
@@ -22,4 +22,16 @@
     Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
         Me.Close()
     End Sub
+
+    Private Sub lnkWebsite_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles lnkWebsite.LinkClicked
+        System.Diagnostics.Process.Start("https://sites.google.com/site/raymondellis89/software/flowgraph")
+    End Sub
+
+    Private Sub lnkGoogleCode_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles lnkGoogleCode.LinkClicked
+        System.Diagnostics.Process.Start("https://code.google.com/p/flowgraph/")
+    End Sub
+
+    Private Sub lnkBugs_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles lnkBugs.LinkClicked
+        System.Diagnostics.Process.Start("https://code.google.com/p/flowgraph/issues/list")
+    End Sub
 End Class
