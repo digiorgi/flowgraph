5a597f55de145d9164b8f08cf98035ac5b9b6681
Raymond Ellis <raymondellis@live.com>
2011-02-28 23:45:29 -0800

    ##LineDrawing## Each object has an random color for now. Makes it easyer to trace the lines.

--- bin/Plugins/Base/BaseObject.vb
+++ bin/Plugins/Base/BaseObject.vb
@@ -243,6 +243,7 @@ Public MustInherit Class BaseObject
         'g.DrawRectangle(Pens.Yellow, ClientRect)
     End Sub
 
+    Private ConnectorPen As New Pen(Color.FromArgb(Rnd() * 255, Rnd() * 255, Rnd() * 255), 2)
     ''' <summary>
     ''' Draw lines connecting outputs to inputs.
     ''' </summary>
@@ -256,11 +257,16 @@ Public MustInherit Class BaseObject
                     Dim pos2 As PointF = Objects(fd.obj).GetInputPosition(fd.Index)
 
                     If pos1.X < pos2.X And pos1.Y = pos2.Y Then
+                        'Draw line: o1─o2
                         g.DrawLine(ConnectorPen, pos1, pos2)
                     ElseIf pos1.X < pos2.X Then
+                        'Draw line:
+                        'o1┐
+                        '  └o2
+                        Dim xHalf As Single = (pos2.X - pos1.X) * 0.5
                         g.DrawLine(ConnectorPen, pos1, New PointF(pos1.X + ((pos2.X - pos1.X) * 0.5), pos1.Y))
-                        g.DrawLine(ConnectorPen, New PointF(pos1.X + ((pos2.X - pos1.X) * 0.5), pos1.Y), New PointF(pos2.X - ((pos2.X - pos1.X) * 0.5), pos2.Y))
-                        g.DrawLine(ConnectorPen, New PointF(pos2.X - ((pos2.X - pos1.X) * 0.5), pos2.Y), pos2)
+                        g.DrawLine(ConnectorPen, New PointF(pos1.X + xHalf, pos1.Y), New PointF(pos2.X - xHalf, pos2.Y))
+                        g.DrawLine(ConnectorPen, New PointF(pos2.X - xHalf, pos2.Y), pos2)
                     Else
                         g.DrawLine(ConnectorPen, pos1, pos2)
                     End If
