fc7ef08493aaa35f7436bb886a48096aa13a8268
Raymond Ellis <raymondellis@live.com>
2011-07-25 12:01:31 -0700

    v0.1.4 #Plugins# Fixed: MIDI_GetController.vb Added: MIDI_GetPitchWheel.vb

--- bin/Plugins/MIDI/MIDI_GetPitchWheel.vb
+++ bin/Plugins/MIDI/MIDI_GetPitchWheel.vb
@@ -1,6 +1,6 @@
-﻿'AddMenuObject|Get,Plugins.MIDI_GetController|MIDI,Channel Message,Controller
+﻿'AddMenuObject|Get,Plugins.MIDI_GetPitchWheel|MIDI,Pitch wheel
 'AddReferences(Sanford.Slim.dll)
-Public Class MIDI_GetController
+Public Class MIDI_GetPitchWheel
     Inherits BaseObject
 
     Private Enabled As Boolean = True
@@ -9,20 +9,17 @@ Public Class MIDI_GetController
     Private numChannel As New NumericUpDown
     Private WithEvents chkAllChannels As New CheckBox
 
-    Private WithEvents comController As New ComboBox
-    Private Controller As Integer
-
 #Region "Object stuff"
     Public Sub New(ByVal StartPosition As Point, ByVal UserData As String)
         Setup(UserData, StartPosition, 200, 50) 'Setup the base rectangles.
 
         'Create one output.
-        Outputs(New String() {"Value,0-1Normalized"})
+        Outputs(New String() {"Value,0-1Normalized", "Value2,0-1Normalized"})
         Inputs(New String() {"Enable,Boolean", "Channel Message,ChannelMessage,ChannelMessageBuilder"})
 
         'Set the title.
-        Title = "Get controller"
-        File = "MIDI\MIDI_GetController.vb"
+        Title = "Get pitch wheel"
+        File = "MIDI\MIDI_GetPitchWheel.vb"
 
         numChannel.Minimum = 1
         numChannel.Maximum = 16
@@ -37,19 +34,11 @@ Public Class MIDI_GetController
         chkAllChannels.Location = Position + New Point(86, 24)
         AddControl(chkAllChannels)
 
-        comController.Width = 200
-        comController.Location = Position
-        comController.DropDownStyle = ComboBoxStyle.DropDownList
-        comController.Items.AddRange([Enum].GetNames(GetType(Sanford.Multimedia.Midi.ControllerType)))
-        comController.SelectedItem = Sanford.Multimedia.Midi.ControllerType.HoldPedal1.ToString
-        AddControl(comController)
-
     End Sub
 
     Public Overrides Sub Dispose()
         numChannel.Dispose()
         chkAllChannels.Dispose()
-        comController.Dispose()
 
         MyBase.Dispose()
     End Sub
@@ -57,7 +46,6 @@ Public Class MIDI_GetController
     Public Overrides Sub Moving()
         numChannel.Location = Position + New Point(45, 25)
         chkAllChannels.Location = Position + New Point(86, 24)
-        comController.Location = Position
     End Sub
 
     Public Overrides Sub Receive(ByVal Data As Object, ByVal sender As DataFlow)
@@ -76,8 +64,9 @@ Public Class MIDI_GetController
                     End If
                 End If
 
-                If Data.Command = Sanford.Multimedia.Midi.ChannelCommand.Controller And Data.Data1 = Controller Then
-                    Send(Data.Data2 / 127)
+                If Data.Command = Sanford.Multimedia.Midi.ChannelCommand.PitchWheel Then
+                    Send(Data.Data1 / 127, 0)
+                    Send(Data.Data2 / 127, 1)
                 End If
         End Select
     End Sub
@@ -92,12 +81,6 @@ Public Class MIDI_GetController
 
         g.GetValue("Enabled", Enabled, False)
 
-        Dim tmpController As Integer = -1
-        g.GetValue("Controller", tmpController, False)
-        If Not tmpController = -1 Then
-            comController.SelectedItem = [Enum].GetName(GetType(Sanford.Multimedia.Midi.ControllerType), tmpController)
-        End If
-
         g.GetValue("AllChannels", chkAllChannels.Checked, False)
 
         g.GetValue("Channel", numChannel.Value, False)
@@ -109,7 +92,6 @@ Public Class MIDI_GetController
         Dim g As SimpleD.Group = MyBase.Save()
 
         g.SetValue("Enabled", Enabled)
-        g.SetValue("Controller", Controller)
         g.SetValue("AllChannels", chkAllChannels.Checked)
         g.SetValue("Channel", numChannel.Value)
 
@@ -121,11 +103,6 @@ Public Class MIDI_GetController
 #Region "Control events"
     Private Sub chkAllChannels_CheckedChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles chkAllChannels.CheckedChanged
         numChannel.Enabled = Not chkAllChannels.Checked
-    End Sub
-
-    Private Sub comController_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles comController.SelectedIndexChanged
-        If comController.SelectedIndex = -1 Then Return
-        Controller = [Enum].Parse(GetType(Sanford.Multimedia.Midi.ControllerType), comController.SelectedItem.ToString)
     End Sub
 #End Region
 
