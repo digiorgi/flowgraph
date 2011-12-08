102139af5b5f345b1d00a80626a16685aea926a5
Raymond Ellis <raymondellis@live.com>
2011-12-07 16:05:27 -0800

    v0.1.4 Added math_Normalizer.vb Can now cancel closing. DisplayAsString was not redrawing properly after resizing, because string size changed.

---
+++ bin/Plugins/Math/math_Normalizer.vb
@@ -0,0 +1,178 @@
+﻿'AddMenuObject|Normalizer,Plugins.math_Normalizer,,|Math
+'AddMenuObject|Denormalizer,Plugins.math_Denormalizer,,|Math
+'Needed|Name displayed,Class name,Width,UserData|Groups
+
+
+Public Class math_Normalizer
+    Inherits BaseObject
+
+    Private Max As Single = 100
+    Private Value As Single = 0
+
+    Private WithEvents txtMax As New TextBox
+
+    Public Sub New(ByVal StartPosition As Point, ByVal UserData As String)
+        Setup(UserData, StartPosition, 50) 'Setup the base rectangles.
+
+
+
+        'Create the inputs.
+        Inputs(New String() {"Value,Number", "Max,Number"})
+        'Create the output.
+        Outputs(New String() {"Value,0-1Normalized"})
+
+        'Set the title.
+        Title = "Normalizer"
+        File = "Math\math_Normalizer.vb"
+
+        txtMax.Text = Max
+        txtMax.Width = 50
+
+        AddControl(txtMax)
+
+        Moving()
+    End Sub
+
+    Public Overrides Sub Moving()
+        txtMax.Location = Position + New Point(0, 10)
+    End Sub
+
+    Public Overrides Sub Receive(ByVal Data As Object, ByVal sender As DataFlow)
+
+        Select Case sender.Index
+            Case 0 'Value
+                Value = CSng(Data)
+                Send(Value / Max)
+
+            Case 1 'Max
+                txtMax.Text = Data.ToString
+
+        End Select
+    End Sub
+
+    Public Overrides Sub Load(g As SimpleD.Group)
+        MyBase.Load(g)
+        g.GetValue("value", Value, False)
+        g.GetValue("max", Max, False)
+        txtMax.Text = Max
+    End Sub
+    Public Overrides Function Save() As SimpleD.Group
+        Dim g As SimpleD.Group = MyBase.Save()
+        g.AddValue("Value", Value)
+        g.AddValue("Max", Max)
+        Return g
+    End Function
+
+
+
+    Private Sub txtMax_KeyUp(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles txtMax.KeyUp
+        If Single.TryParse(txtMax.Text, Max) = False Then
+            MsgBox("Needs to be proper number!", MsgBoxStyle.Exclamation, "Normalizer - Flowgraph")
+            Return
+        End If
+        If e.KeyCode = Keys.Enter Then
+            Send(Value / Max)
+        End If
+    End Sub
+
+    Private Sub txtMax_TextChanged(sender As Object, e As System.EventArgs) Handles txtMax.TextChanged
+        Single.TryParse(txtMax.Text, Max)
+    End Sub
+End Class
+
+Public Class math_Denormalizer
+    Inherits BaseObject
+
+    Private Max As Single = 100
+    Private Value As Single = 0
+
+    Private WithEvents txtMax As New TextBox
+    Private WithEvents chkRound As New CheckBox
+
+    Public Sub New(ByVal StartPosition As Point, ByVal UserData As String)
+        Setup(UserData, StartPosition, 60) 'Setup the base rectangles.
+
+
+
+        'Create the inputs.
+        Inputs(New String() {"Value,0-1Normalized", "Max,Number"})
+        'Create the output.
+        Outputs(New String() {"Value,Number"})
+
+        'Set the title.
+        Title = "Denormalizer"
+        File = "Math\math_Normalizer.vb"
+
+        txtMax.Text = Max
+        txtMax.Width = Size.Width
+        AddControl(txtMax)
+
+        chkRound.Text = "Round"
+        chkRound.Width = Size.Width
+        chkRound.Height = 12
+        AddControl(chkRound)
+
+
+        Moving()
+    End Sub
+
+    Public Overrides Sub Moving()
+        chkRound.Location = Position + New Point(0, -2)
+        txtMax.Location = Position + New Point(0, 10)
+    End Sub
+
+    Public Overrides Sub Receive(ByVal Data As Object, ByVal sender As DataFlow)
+
+        Select Case sender.Index
+            Case 0 'Value
+                value = CSng(Data)
+                SendValue()
+
+
+            Case 1 'Max
+                txtMax.Text = Data.ToString
+
+        End Select
+    End Sub
+
+    Public Overrides Sub Load(g As SimpleD.Group)
+        MyBase.Load(g)
+        g.GetValue("value", Value, False)
+        g.GetValue("max", Max, False)
+        txtMax.Text = Max
+        g.GetValue("round", chkRound.Checked, False)
+    End Sub
+    Public Overrides Function Save() As SimpleD.Group
+        Dim g As SimpleD.Group = MyBase.Save()
+        g.AddValue("Value", Value)
+        g.AddValue("Max", Max)
+        g.AddValue("Round", chkRound.Checked.ToString)
+        Return g
+    End Function
+
+    Private Sub SendValue()
+        If chkRound.Checked Then
+            Send(Math.Round(Value * Max))
+        Else
+            Send(Value * Max)
+        End If
+    End Sub
+
+    Private Sub txtMax_KeyUp(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles txtMax.KeyUp
+        If Single.TryParse(txtMax.Text, Max) = False Then
+            MsgBox("Needs to be proper number!", MsgBoxStyle.Exclamation, "Normalizer - Flowgraph")
+            Return
+        End If
+        If e.KeyCode = Keys.Enter Then
+            SendValue()
+        End If
+    End Sub
+
+    Private Sub txtMax_TextChanged(sender As Object, e As System.EventArgs) Handles txtMax.TextChanged
+        Single.TryParse(txtMax.Text, Max)
+    End Sub
+
+    Private Sub chkRound_CheckedChanged(sender As Object, e As System.EventArgs) Handles chkRound.CheckedChanged
+        SendValue()
+    End Sub
+End Class
